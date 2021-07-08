ecca7afd5a7424de7fb8400f52108a2445caa744
vdzhyranov <v.dzhyranov@gmail.com>
2021-07-08 22:19:28 +0300

    a

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,21 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const newUl = document.createElement('ul');
+
+  for (const item in data) {
+    const dataElement = document.createElement('li');
+
+    dataElement.append([item]);
+    newUl.append(dataElement);
+
+    if (typeof data[item] === 'object'
+      && data[item] !== null) {
+      createTree(dataElement, data[item]);
+    }
+  }
+
+  element.append(newUl);
 }
 
 createTree(tree, food);
